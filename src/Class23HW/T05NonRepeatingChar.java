af98d848b88da4b0a061db6f7b82ca02381ea028
LaisLatifi <mudaser1992@gmail.com>
2023-05-28 00:09:59 -0700

    My class 18 homework

---
+++ src/Class23HW/T05NonRepeatingChar.java
@@ -0,0 +1,25 @@
+package Class23HW;
+
+
+//TASK#05
+    // Find the First Non-Repeating Character in a String: Given a string,
+// find and return the first non-repeating character. For example, in the
+// string "abracadabra", the first non-repeating character is 'c'.
+    public class T05NonRepeatingChar {
+        public char findFirstNonRepeat(String str) {
+            char c = ' ';
+            for (int i = 0; i < str.length(); i++) {
+                if (str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i))) {
+                    c = str.charAt(i);
+                    break;
+                }
+            }
+            return c;
+        }
+
+        public static void main(String[] args) {
+            var obj = new T05NonRepeatingChar();
+            System.out.println(obj.findFirstNonRepeat("abracadabra"));
+        }
+    }
+
