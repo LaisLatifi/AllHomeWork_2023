df7775be00f23d6761deb21c94d9e79cca793423
LaisLatifi <mudaser1992@gmail.com>
2023-05-28 00:06:38 -0700

    My class 18 homework

---
+++ src/Class23HW/T03NoOfWords.java
@@ -0,0 +1,21 @@
+package Class23HW;
+
+    //Task#03
+    // Count the Number of Words in a String: Write a function to count the
+//  number of words in a given string. Words are separated by spaces or
+//  punctuation. For example, the input "Hello, world!" should return 2.
+    public class T03NoOfWords {
+        int noOfWords(String str) {
+            int count = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == ' ') count++;
+            }
+            return count + 1;
+        }
+
+        public static void main(String[] args) {
+            var obj = new T03NoOfWords();
+            System.out.println(obj.noOfWords("This is a sentence, yo!"));
+        }
+    }
+
